e94e46759108b3518d845e077f44a6eb6451ded8
Patrick Amartey Annang <102452248+amartey-tricky@users.noreply.github.com>
2024-01-04 20:01:56 +0000

    integrate sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import sentry from "@sentry/astro";
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
   adapter: vercel(),
+  integrations: [
+    sentry({
+      dsn: "https://2265045c8241dcc41707bff4bfa67f78@o4506514612813824.ingest.sentry.io/4506514688966656",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
+  ],
 });
